35306454c548514961b6c8e12b3d3861dd05934e
Pixayo <kaio.rodrigo729@gmail.com>
2026-09-04 09:04:53 -0300

    update: n command

--- n.sh
+++ n.sh
@@ -8,13 +8,14 @@ set -euo pipefail
 #
 # If no extra argument, opens current directory
 
-path=$(realpath -m -- "$1")
+path=$(realpath -m -- "${$1:-.}")
 
 if [[ -d "$path" ]]; then
   xdg-open $path
 else
   echo "not found: $path"
   echo "nothing was done"
+  exit 1
 fi
 
 # man: xdg-open opens a file or URL in the user's preferred application. [...]
